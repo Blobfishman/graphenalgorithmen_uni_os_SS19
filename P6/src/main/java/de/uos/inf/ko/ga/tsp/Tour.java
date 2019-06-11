83e5d154bf91b1d3fff81b7a03cf043eeb8d868b
Eric <erneumann@uni-osnabrueck.de>
2019-06-11 20:36:46 +0200

    P6

---
+++ P6/src/main/java/de/uos/inf/ko/ga/tsp/Tour.java
@@ -0,0 +1,126 @@
+package de.uos.inf.ko.ga.tsp;
+
+import de.uos.inf.ko.ga.graph.Graph;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Class representing a tour in a graph.
+ * @author Tobias Oelschlägel
+ *
+ */
+public class Tour {
+
+	private final Graph g;
+	private final int[] vertices;
+
+	/**
+	 * Initializes an empty tour
+	 * @param g Graph
+	 */
+	public Tour(Graph g) {
+		this(g, null);
+	}
+
+	/**
+	 * Creates a copy of a tour.
+	 * @param tour Tour to be copied
+	 */
+	public Tour(Tour tour) {
+		this(tour.getGraph(), tour.getVertices());
+	}
+
+	/**
+	 * Initializes a tour with a given order of the vertices.
+	 * @param g Graph
+	 * @param vertices Order of the vertices
+	 */
+	public Tour(Graph g, int[] vertices) {
+		this.g = g;
+
+		if (vertices != null) {
+			this.vertices = new int[vertices.length];
+			System.arraycopy(vertices, 0, this.vertices, 0, vertices.length);
+		} else {
+			this.vertices = new int[0];
+		}
+	}
+
+	public static Tour[] randomTour(Graph g)
+	{
+		Tour[] randomTours = new Tour[100];
+
+		for (int i = 0; i < 100; i++)
+		{
+			List<Integer> knoten = new ArrayList<>();
+			int number = g.getVertexCount();
+
+			for (int k = 0; k < number; k++)
+			{
+				knoten.add(i);
+			}
+
+			int[] random = new int[6];
+
+			for (int j = 0; j < 6; j++)
+			{
+				int next = (int) (Math.random() * knoten.size());
+				random[j] = next;
+				knoten.remove(next);
+			}
+
+			randomTours[i] = new Tour(g,random);
+		}
+
+		return randomTours;
+	}
+
+	/**
+	 * Computes the total costs of the tour.
+	 * @return total sum of costs of edges contained in the tour
+	 */
+	public double getCosts() {
+		double costs = 0.0;
+		
+		final int n = this.vertices.length;
+
+		for (int i = 0; i < n; ++i) {
+			costs += this.g.getEdgeWeight(this.vertices[i], this.vertices[(i + 1) % n]);
+		}
+		
+		return costs;
+	}
+
+	/**
+	 * Gets the order of the vertices of the tour.
+	 * @return order of vertices
+	 */
+	public int[] getVertices() {
+		return this.vertices;
+	}
+
+	/**
+	 * Gets the graph.
+	 * @return graph
+	 */
+	public Graph getGraph() {
+		return this.g;
+	}
+	
+	@Override
+	public String toString() {
+		final StringBuilder s = new StringBuilder();
+		for (int i = 0; i < this.vertices.length; i++){
+			s.append(this.vertices[i]);
+			s.append(" -> ");
+		}
+		
+		s.append(this.vertices[0]);
+		s.append(", Kosten = ");
+		s.append(this.getCosts());
+
+		return s.toString();
+	}
+}
+ 
---
+++ P6/src/main/java/de/uos/inf/ko/ga/tsp/TwoOpt.java
@@ -0,0 +1,148 @@
+package de.uos.inf.ko.ga.tsp;
+
+import de.uos.inf.ko.ga.graph.Graph;
+
+import java.util.*;
+import java.math.*;
+
+public class TwoOpt {
+
+	/**
+	 * Perform a two-opt exchange step of the edges (v[pos1], v[pos1 + 1]) and (v[pos2], v[pos2 + 1]).
+	 * The resulting tour visits
+	 *   v[0], ..., v[pos1], v[pos2], v[pos2 - 1], ..., v[pos1 + 1], v[pos2 + 1], v[pos2 + 2], ..., v[n - 1].
+	 * Special case: if the last edge (v[n - 1], v[0]) is used, then the following tour is created:
+	 *   v[0], v[pos1 + 1], v[pos1 + 2], ..., v[n - 1], v[pos1], v[pos1 - 1], ..., v[0]
+	 * @param tour Tour
+	 * @param pos1 Index of the starting vertex of the first edge
+	 * @param pos2 Index of the starting vertex of the second edge
+	 * @return tour obtained by performing the edge exchange
+	 */
+	public static Tour twoOptExchange(Tour tour, int pos1, int pos2) {
+		assert(tour != null);
+		assert(pos1 >= 0);
+		assert(pos2 > pos1 + 1);
+		assert(pos2 < tour.getVertices().length);
+		assert(pos1 != (pos2 + 1) % tour.getVertices().length);
+		// throw new UnsupportedOperationException("method not implemented");
+
+
+		boolean pos2tonull = false;
+		int pos1suc = pos2;
+		int pos2suc = pos1;
+
+		if (pos2 == tour.getVertices().length - 1)
+		{
+			pos2tonull = true;
+		}
+
+		int[] vertices = tour.getVertices();
+		int[] vertices2 = vertices;
+
+		if (!pos2tonull) {
+
+			int i = pos1 + 1;
+			while (pos1suc != pos1 + 1) {
+				vertices[i] = vertices2[pos1suc];
+				pos1suc--;
+				i++;
+			}
+
+			while (pos2 != vertices.length - 1) {
+				vertices[i] = vertices2[pos2 + 1];
+				pos2++;
+				i++;
+			}
+		}
+		else {
+			int i = 1;
+			while (pos1 != vertices.length -1) {
+				vertices[i] = vertices2[pos1 +1];
+				pos1++;
+				i++;
+			}
+
+			while (pos2suc != -1)
+			{
+				vertices[i] = vertices2[pos2suc];
+				pos2suc--;
+				i++;
+			}
+		}
+
+		return new Tour(tour.getGraph(),vertices);
+	}
+
+	/**
+	 * Single step of the Two-Opt neighborhood for the TSP with either
+	 * first-fit or best-fit selection of the neighbor.
+	 * - First-fit returns the first neighbor that is found that has a
+	 *   better objective value than the original tour.
+	 * - Best-fit always searches through the whole neighborhood and
+	 *   returns one of the tours with the best objective values.
+	 * @param tour
+	 * @param firstFit whether to use first-fit or best-fit for neighbor selection
+	 * @return tour obtained by performing the first or the best improvement
+	 */
+	public static Tour twoOptNeighborhood(Tour tour, boolean firstFit) {
+		//throw new UnsupportedOperationException("method not implemented");
+
+		Tour best_tour = tour;
+		Tour new_tour = tour;
+
+		if(!firstFit)
+		{
+			for (int i = 0; i < tour.getGraph().getVertexCount(); i++) {
+
+				if (i <= 3) {
+					for (int x = 0; x <= i - 2; x++) {
+						new_tour = twoOptExchange(tour, x, i);
+						if (new_tour.getCosts() < best_tour.getCosts()) {
+							best_tour = new_tour;
+						}
+					}
+				}
+
+				for (int x = i + 2; x < tour.getGraph().getVertexCount(); x++) {
+					new_tour = twoOptExchange(tour, i, x);
+					if(new_tour.getCosts() < best_tour.getCosts())
+					{
+						best_tour = new_tour;
+					}
+				}
+			}
+
+			return new_tour;
+
+		}
+		else {
+
+		}
+
+
+
+
+	}
+
+	/**
+	 * Iterative Two-Opt neighborhood for the TSP.
+	 * This method calls twoOptNeighborhood iteratively as long as the
+	 * tour can be improved.
+	 * @param tour Tour to be improved
+	 * @param firstFit whether to use first-fit or best-fit for neighbor selection
+	 * @return best tour obtained by iteratively applying the two-opt neighborhood
+	 */
+	public static Tour iterativeTwoOpt(Tour tour, boolean firstFit) {
+
+		if (!firstFit){
+			twoOptNeighborhood(tour, false);
+		}
+		else {
+			twoOptNeighborhood(tour, true);
+		}
+		throw new UnsupportedOperationException("method not implemented");
+	}
+
+
+
+}
